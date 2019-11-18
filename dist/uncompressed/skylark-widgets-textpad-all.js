2d2e31b447a7ff0be2836698d58a03bc85583d76
hudaokeji <github@hudaokeji.com>
2019-11-18 23:06:41 +0900

    update libraries

--- dist/uncompressed/skylark-widgets-textpad-all.js
+++ dist/uncompressed/skylark-widgets-textpad-all.js
@@ -2810,10 +2810,12 @@ define('skylark-langx-emitter/Emitter',[
             }
 
             return this;
+        },
+
+        trigger  : function() {
+            return this.emit.apply(this,arguments);
         }
     });
-
-    Emitter.prototype.trigger = Emitter.prototype.emit;
 
     Emitter.createEvent = function (type,props) {
         var e = new CustomEvent(type,props);
@@ -4097,7 +4099,7 @@ define('skylark-domx-noder/noder',[
                 node.appendChild(html);
             }
 
-
+            return this;
         }
     }
 
@@ -11039,10 +11041,12 @@ define('skylark-widgets-base/Widget',[
 
               }
           }
-
-
         }
 
+        if (this._elm.parentElement) {
+          // The widget is already in document
+          this._startup();
+        }
 
      },
 
@@ -15254,9 +15258,9 @@ define('skylark-codemirror/primitives/display/update_display',[
         if (updateDisplayIfNeeded(cm, update)) {
             n.updateHeightsInViewport(cm);
             postUpdateDisplay(cm, update);
-            let barMeasure = l.measureForScrollbars(cm);
+            let barMeasure = cm.measureForScrollbars(); //l.measureForScrollbars(cm);
             m.updateSelection(cm);
-            l.updateScrollbars(cm, barMeasure);
+            cm.updateScrollbars(barMeasure); // l.updateScrollbars(cm, barMeasure);
             setDocumentHeight(cm, barMeasure);
             update.finish();
         }
@@ -15609,11 +15613,11 @@ define('skylark-codemirror/primitives/display/scrollbars',[
     './update_lines',
     '../util/misc',
     './scrolling'
-], function (a, b, c, d, e, f, g) {
+], function (dom, event, position_measurement, browser, update_lines, misc, scrolling) {
     'use strict';
     function measureForScrollbars(cm) {
         let d = cm.display, gutterW = d.gutters.offsetWidth;
-        let docH = Math.round(cm.doc.height + c.paddingVert(cm.display));
+        let docH = Math.round(cm.doc.height + position_measurement.paddingVert(cm.display));
         return {
             clientHeight: d.scroller.clientHeight,
             viewHeight: d.wrapper.clientHeight,
@@ -15622,7 +15626,7 @@ define('skylark-codemirror/primitives/display/scrollbars',[
             viewWidth: d.wrapper.clientWidth,
             barLeft: cm.options.fixedGutter ? gutterW : 0,
             docHeight: docH,
-            scrollHeight: docH + c.scrollGap(cm) + d.barHeight,
+            scrollHeight: docH + position_measurement.scrollGap(cm) + d.barHeight,
             nativeBarWidth: d.nativeBarWidth,
             gutterWidth: gutterW
         };
@@ -15630,21 +15634,21 @@ define('skylark-codemirror/primitives/display/scrollbars',[
     class NativeScrollbars {
         constructor(place, scroll, cm) {
             this.cm = cm;
-            let vert = this.vert = a.elt('div', [a.elt('div', null, null, 'min-width: 1px')], 'CodeMirror-vscrollbar');
-            let horiz = this.horiz = a.elt('div', [a.elt('div', null, null, 'height: 100%; min-height: 1px')], 'CodeMirror-hscrollbar');
+            let vert = this.vert = dom.elt('div', [dom.elt('div', null, null, 'min-width: 1px')], 'CodeMirror-vscrollbar');
+            let horiz = this.horiz = dom.elt('div', [dom.elt('div', null, null, 'height: 100%; min-height: 1px')], 'CodeMirror-hscrollbar');
             vert.tabIndex = horiz.tabIndex = -1;
             place(vert);
             place(horiz);
-            b.on(vert, 'scroll', () => {
+            event.on(vert, 'scroll', () => {
                 if (vert.clientHeight)
                     scroll(vert.scrollTop, 'vertical');
             });
-            b.on(horiz, 'scroll', () => {
+            event.on(horiz, 'scroll', () => {
                 if (horiz.clientWidth)
                     scroll(horiz.scrollLeft, 'horizontal');
             });
             this.checkedZeroWidth = false;
-            if (d.ie && d.ie_version < 8)
+            if (browser.ie && browser.ie_version < 8)
                 this.horiz.style.minHeight = this.vert.style.minWidth = '18px';
         }
         update(measure) {
@@ -15693,11 +15697,11 @@ define('skylark-codemirror/primitives/display/scrollbars',[
                 this.enableZeroWidthBar(this.vert, this.disableVert, 'vert');
         }
         zeroWidthHack() {
-            let w = d.mac && !d.mac_geMountainLion ? '12px' : '18px';
+            let w = browser.mac && !browser.mac_geMountainLion ? '12px' : '18px';
             this.horiz.style.height = this.vert.style.width = w;
             this.horiz.style.pointerEvents = this.vert.style.pointerEvents = 'none';
-            this.disableHoriz = new f.Delayed();
-            this.disableVert = new f.Delayed();
+            this.disableHoriz = new misc.Delayed();
+            this.disableVert = new misc.Delayed();
         }
         enableZeroWidthBar(bar, delay, type) {
             bar.style.pointerEvents = 'auto';
@@ -15738,7 +15742,7 @@ define('skylark-codemirror/primitives/display/scrollbars',[
         updateScrollbarsInner(cm, measure);
         for (let i = 0; i < 4 && startWidth != cm.display.barWidth || startHeight != cm.display.barHeight; i++) {
             if (startWidth != cm.display.barWidth && cm.options.lineWrapping)
-                e.updateHeightsInViewport(cm);
+                update_lines.updateHeightsInViewport(cm);
             updateScrollbarsInner(cm, measureForScrollbars(cm));
             startWidth = cm.display.barWidth;
             startHeight = cm.display.barHeight;
@@ -15771,23 +15775,23 @@ define('skylark-codemirror/primitives/display/scrollbars',[
         if (cm.display.scrollbars) {
             cm.display.scrollbars.clear();
             if (cm.display.scrollbars.undefined)
-                a.rmClass(cm.display.wrapper, cm.display.scrollbars.undefined);
+                dom.rmClass(cm.display.wrapper, cm.display.scrollbars.undefined);
         }
         cm.display.scrollbars = new scrollbarModel[cm.options.scrollbarStyle](node => {
             cm.display.wrapper.insertBefore(node, cm.display.scrollbarFiller);
-            b.on(node, 'mousedown', () => {
+            event.on(node, 'mousedown', () => {
                 if (cm.state.focused)
                     setTimeout(() => cm.display.input.focus(), 0);
             });
             node.setAttribute('cm-not-content', 'true');
         }, (pos, axis) => {
             if (axis == 'horizontal')
-                g.setScrollLeft(cm, pos);
+                scrolling.setScrollLeft(cm, pos);
             else
-                (cm, pos);
+                scrolling.updateScrollTop(cm, pos);
         }, cm);
         if (cm.display.scrollbars.undefined)
-            a.addClass(cm.display.wrapper, cm.display.scrollbars.undefined);
+            dom.addClass(cm.display.wrapper, cm.display.scrollbars.undefined);
     }
     return {
         measureForScrollbars: measureForScrollbars,
