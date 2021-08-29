4c8b2b6247a46403743bb23f7fb29609a14af426
hudaokeji <github@hudaokeji.com>
2021-08-29 21:53:22 +0900

    rename module file name

--- dist/skylark-widgets-textpad/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-widgets-textpad - The skylark text editor widget
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-widgets/skylark-widgets-textpad/
- * @license MIT
- */
-define(["./textpad","./Editor"],function(t){return t});
-//# sourceMappingURL=sourcemaps/main.js.map
--- dist/uncompressed/skylark-widgets-textpad/main.js
+++
@@ -1,6 +0,0 @@
-define([
-    "./textpad",
-    "./Editor"
-], function(textpad) {
-    return textpad;
-});
